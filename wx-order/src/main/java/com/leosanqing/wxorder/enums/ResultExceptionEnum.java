a1f2aaa428ce5825db61f9575131102e887f1a4b
leosanqing <stormleo@qq.com>
2019-07-28 23:27:33 +0800

    update

--- wx-order/src/main/java/com/leosanqing/wxorder/enums/ResultExceptionEnum.java
+++ wx-order/src/main/java/com/leosanqing/wxorder/enums/ResultExceptionEnum.java
@@ -8,7 +8,9 @@ public enum ResultExceptionEnum {
     PRODUCT_NOT_EXIT(10,"商品不存在"),
     PRODUCT_STOCK_ERROR(11,"库存不足"),
     ORDER_NOT_EXIT(12,"订单不存在"),
-    ORDER_DETAIL_NOT_EXIT(3,"订单详情不存在"),
+    ORDER_DETAIL_NOT_EXIT(13,"订单详情不存在"),
+    ORDER_STATUS_ERROR(14,"订单状态不正确"),
+    ORDER_UPDATE_ERROR(15,"订单更新失败"),
     ;
 
     ResultExceptionEnum(Integer code, String mag) {
