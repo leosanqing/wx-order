3569267cbb0a91a0bb428ee8372debb9094465a5
leosanqing <stormleo@qq.com>
2019-07-31 20:53:58 +0800

    update BuyerOrderContorller

--- wx-order/src/main/java/com/leosanqing/wxorder/controller/BuyerOrderController.java
+++ wx-order/src/main/java/com/leosanqing/wxorder/controller/BuyerOrderController.java
@@ -1,19 +1,24 @@
 package com.leosanqing.wxorder.controller;
 
 import com.leosanqing.wxorder.VO.ResultVO;
+import com.leosanqing.wxorder.converter.OrderForm2OrderDTO;
 import com.leosanqing.wxorder.dto.OrderDTO;
 import com.leosanqing.wxorder.enums.ResultExceptionEnum;
 import com.leosanqing.wxorder.exception.SellException;
 import com.leosanqing.wxorder.form.OrderForm;
 import com.leosanqing.wxorder.service.OrderService;
+import com.leosanqing.wxorder.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -39,10 +44,18 @@ public class BuyerOrderController {
                     bindingResult.getFieldError().getDefaultMessage());
         }
 
-        OrderDTO orderDTO = new OrderDTO();
+        OrderDTO orderDTO = OrderForm2OrderDTO.convert(orderForm);
 
 
-        orderService.create();
+        if(CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+            log.error("[转换异常] 购物车为空");
+            throw new SellException(ResultExceptionEnum.CART_EMPTY);
+        }
+        OrderDTO orderResult = orderService.create(orderDTO);
+        Map<String,String> hash = new HashMap<>();
+        hash.put("orderId",orderResult.getOrderId());
+        ResultVO success = ResultVOUtil.success(hash);
+        return success;
     }
 
 
